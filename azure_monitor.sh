ae81c1a5570e59e7d85fd5836055d5d3ba6be8a0
dennis di martini ronn <dennis.Ronn@bematech.com.br>
2018-09-18 19:58:06 -0300

    Primeiro Commit

---
+++ azure_monitor.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Active Directory ID
+adid=$2
+#Subscription ID
+subid=$3
+#Service Principal-ADApplication ID
+clientid=$4
+#Servoce Principal-ADApplication Secret
+clientsecret=$5
+#Resource Group Name
+resourcegroup=$6
+#Resource Type
+resourcetype=$7
+#Resource Name
+resourcename=$8
+#Mettric Aggregation
+aggregation=$9
+#Mettric Name
+metricname=${10}
+#granulação de tempo
+timegrain=PT"$1"M
+
+#inicio (hora atual menos 1 minuto)
+start=`date --utc +%Y-%m-%dT%H:%M:00Z -d "$1 min ago"`
+
+#fim (hora atual)
+end=`date --utc +%Y-%m-%dT%H:%M:00Z`
+
+Bearer=(`wget -qO- "https://login.windows.net/$adid/oauth2/token"  --post-data "resource=https://management.core.windows.net&client_id=$clientid&grant_type=client_credentials&client_secret=$clientsecret" | jq -r '.access_token'`)
+
+URL="https://management.azure.com/subscriptions/$subid/resourceGroups/$resourcegroup/providers/$resourcetype/$resourcename/providers/microsoft.insights/metrics?api-version=2018-01-01&timespan=$start/$end&interval=$timegrain&aggregation=$aggregation&metricnames=$metricname"
+
+wget -qO- --header="Authorization: Bearer $Bearer" --header="Content-Type: application/json" $URL | jq  -r ".value[0].timeseries[0].data[0]."$aggregation""
